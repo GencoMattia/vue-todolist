1cc1a7ae4b0db263c19aee6aa20ef4a1a04d114d
GencoMattia <gencomattia@hotmail.it>
2024-05-30 15:45:47 +0200

    js: added item-list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            tasksList: [
+                {
+                    text: "Spada Vorpal+5",
+                    done: false
+                },
+                {
+                    text: "Zaino senza Fondo",
+                    done: false
+                },
+                {
+                    text: "Pozione di cura 2d6",
+                    done: false
+                },
+                {
+                    text: "Anello del Sostentamento",
+                    done: false
+                },
+                {
+                    text: "Tomo del Mago cattivo",
+                    done: false
+                },
+                {
+                    text: "Armatura di Mithril +2",
+                    done: false
+                },
+            ]
         }
     }
 }).mount('#app')
